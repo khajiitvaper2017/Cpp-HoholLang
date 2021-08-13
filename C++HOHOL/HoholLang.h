3345936c8d66eebc16fa1de290fe9a301657fd83
khajiitvaper2017 <kanalgera@gmail.com>
2021-08-13 17:45:26 +0300

    Добавьте файлы проекта.

---
+++ C++HOHOL/HoholLang.h
@@ -0,0 +1,94 @@
+#pragma once
+#define яхохол setlocale(LC_CTYPE, "ukr");
+#define основний main
+#define асемблер asm
+#define сало ;
+
+#define та and
+#define та_рівно and_eq
+#define не not
+#define не_рівно not_eq
+#define біт_виняткабо xor
+#define біт_виняткабо_рівно xor_eq
+#define біт_та bitand
+#define біт_або bitor
+#define біт_інверсія compl
+
+#define логічний bool
+#define символ char
+#define символ16 char16_t
+#define символ32 char32_t
+
+#define цілий int
+#define беззнаковий unsigned
+#define знаковий signed
+#define довгий long
+#define подвійний double
+#define рухомий float
+#define короткий short
+
+#define унія union
+#define клас class
+#define структура struct
+#define перелік enum
+
+#define цей this
+#define новий new
+#define нічого void
+#define закритий private
+#define відкритий public
+#define захищений protected
+#define уявний virtual
+#define змінний mutable
+#define хибний false
+#define істинний true
+
+#define зовнішній extern
+#define нульвказ nullptr
+#define тип auto
+#define непостійний volatile
+#define оператор operator
+#define видалити delete
+#define постійний const
+#define вбудований inline
+#define дружній friend
+
+#define імя_типу typename
+#define визн_типу typedef
+#define ідент_типу typeid
+
+#define розмір sizeof
+#define явний explicit
+
+#define шаблон template
+#define простір_імен namespace
+
+#define використовувати using
+
+#define експорт export
+#define постійн_вираз constexpr
+#define без_вийняток noexcept
+
+#define протягом for
+#define спробувати try
+#define перейти goto
+#define випадок case
+#define робити do
+#define поки while
+#define повернути return
+#define якщо if
+#define інакше else
+#define зупинити break;
+#define продовжити continue;
+#define викинути throw
+#define перемикач switch
+#define піймати catch
+#define зазамовч default
+
+
+#include <iostream>
+#define стнд std
+#define строка std::string
+#define вивід std::cout
+#define ввід std::cin
+#define кін_стр std::endl
